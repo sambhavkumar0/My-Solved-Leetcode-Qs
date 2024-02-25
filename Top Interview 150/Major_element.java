bbcae31b320e8dc0bd0c8d93925fec39dfc81f4c
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-02-25 09:55:02 +0530

    major element java

---
+++ Top Interview 150/Major_element.java
@@ -0,0 +1,23 @@
+class Major_element{
+    public static int major(int[] nums){
+        int major=nums[0],count=0;
+        for(int i=1;i<nums.length;i++){
+            if(count==0){
+                count++;
+                major=nums[i];
+            }
+            else if(major==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return major;
+    }
+    public static void main(String[] args) {
+        int nums[]={2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
+        int result=major(nums);
+        System.out.println("the major element is "+result);
+    }
+}
