fab400707bed53615245d9d9e4fb90c23f06443b
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-02-25 10:05:52 +0530

    Rotate array java

--- Top Interview 150/Major_element.java
+++ Top Interview 150/Major_element.java
@@ -18,7 +18,7 @@ class Major_element{
         return major;
     }
     public static void main(String[] args) {
-        int nums[]={2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};
+        int nums[]={2,2,2,2,2,2,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4};//expected outcome is 4
         int result=major(nums);
         System.out.println("the major element is "+result);
     }
