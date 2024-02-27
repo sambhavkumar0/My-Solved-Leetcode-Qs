776ae801aaaaff4b4da0803f062f6cdeab9ebcd6
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-02-27 22:38:16 +0530

    errors fixed with new solution

--- Top Interview 150/Answer.java
+++ Top Interview 150/Answer.java
@@ -1,30 +1,39 @@
 //given array nums: return answer such that answer[i] is equal to product of all elements of nums except nums[i]..
 
 public class Answer {
-    public static int[] productExceptSelf(int[] nums) {
+    public static void productExceptSelf(int[] nums) {
 
-        int n=nums.length;
-        int[] res=new int[n];
-        res[0]=1;
-        for(int i=1;i<n;i++){
-            res[i]=res[i-1]*nums[i-1];
-
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
+        /**
+          calculate the left Prefix and right Suffix Product.
+        */
+        for (int i=0,j= nums.length-1; i < nums.length; i++, j--) {
+            if (i == 0) {
+                leftProduct[i] = nums[i];
+                rightProduct[j] = nums[j];
+            }else {
+                leftProduct[i] = leftProduct[i-1] * nums[i];
+                rightProduct[j] = rightProduct[j+1] * nums[j];
+            }
         }
-        int right=1;
-        for(int i=n-1;i>=0;i--){
-            res[i]=res[i]*right;
-            right=right*nums[i];
-    }
-    return res;
+    
+        for (int i=0; i < nums.length; i++) {
+    
+            if (i == 0) {
+                nums[i] = rightProduct[1];
+            }else if (i == (nums.length - 1)) {
+                nums[i] = leftProduct[i-1];
+            }else {
+                nums[i] = leftProduct[i-1] * rightProduct[i+1];
+            }
+        }
+       for(int i=0; i<nums.length; i++) {
+            System.out.print(nums[i] + " ,");
+       }
 }
 public static void main(String[] args) {
-    int[] nums={2,4,6,8};
-    int[] j=new int[nums.length];
-    for(int i=0; i<nums.length; i++) {
-       j= productExceptSelf(nums);
+    int[] nums={1,2,3,4};//expected output:24,12,8,6
+    productExceptSelf(nums);
 
-}
-for(int k=0; k<nums.length; k++) {
-    System.out.println(j[k]);
-}
 }}
