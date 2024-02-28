b81c6ffc13426d8a579ceb6ca835eff949e36174
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-02-28 22:59:56 +0530

    valid parenthesis java

---
+++ Top Interview 150/Valid_p.java
@@ -0,0 +1,24 @@
+//Valid parenthesis: every open bracket must have a closed bracket
+import java.util.*;
+public class Valid_p {
+    public static boolean isValid(String s){
+        Stack<Character> stack=new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(c=='(')
+            stack.push(')');
+            else if(c=='{')
+            stack.push('}');
+            else if(c=='[')
+            stack.push(']');
+            else if(stack.isEmpty()||stack.pop()!=c) 
+            return false;
+        }
+        return stack.isEmpty();
+
+    }
+    public static void main(String[] args) {
+        String s="({lol})";
+        boolean isValid = isValid(s);
+        System.out.println("the parenthesis is valid or not?????? the answer is "+isValid);
+    }
+}
